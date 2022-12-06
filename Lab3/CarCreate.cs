cc25b2ec414bfd60f5f5f09323a754c47d04639e
User <User@DESKTOP-RPSJ6FD>
2022-12-06 17:48:00 +0200

    Add project files.

---
+++ Lab3/CarCreate.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Car
+{
+    public const string CompanyName = "Havi-Motors";
+    private string name;
+    private string model;
+    private string color;
+    private double price;
+
+    public string Color
+    {
+        get { return color; }
+        set { color = value; }
+    }
+
+    public Car() : this("Name", "Model", "White", 10000)
+    {
+    }
+
+    public Car(string name, string model, string color, int price)
+    {
+        this.name = name;
+        this.model = model;
+        this.Color = color;
+        this.price = price;
+    }
+
+    public void Input()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine("Введіть марку машини:");
+        this.name = Console.ReadLine();
+        Console.WriteLine("Введіть модель машини:");
+        this.model = Console.ReadLine();
+        Console.WriteLine("Введіть колір машини:");
+        this.Color = Console.ReadLine();
+        Console.WriteLine("Введіть ціну машини:");
+        this.price = Convert.ToDouble(Console.ReadLine());
+    }
+
+    public void Print()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine($"Company name: {Car.CompanyName} Name: {this.name} " +
+            $"Model: {this.model} Color: {this.Color} Price: {this.price}");
+    }
+
+    public static bool operator ==(Car car1, Car car2)
+    {
+        return (car1.name == car2.name) && (car1.price == car2.price);
+    }
+    public static bool operator !=(Car car1, Car car2)
+    {
+        return (car1.name != car2.name) && (car1.price != car2.price);
+    }
+
+    public override string ToString()
+    {
+        return $"Company name: {Car.CompanyName} Name: {this.name} " +
+            $"Model: {this.model} Color: {this.Color} Price: {this.price}";
+    }
+
+    public void ChangePrice(double x)
+    {
+        price += price * x / 100;
+    }
+}
---
+++ Lab3/Programm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Натисніть любу кнопку для того щоб побачити дані про Car");
+            Console.ReadKey();
+
+            Car car1 = new Car("Audi", "A6", "Black", 14600);
+            Car car2 = new Car("Voltswagen", "Passat B5", "Silver", 12500);
+            Car car3 = new Car("BMW", "528", "Blue", 13400);
+
+            car1.Print();
+            car1.ChangePrice(10);
+            car1.Print();
+
+            car2.Print();
+            car2.ChangePrice(10);
+            car2.Print();
+
+            car3.Print();
+            car3.ChangePrice(10);
+            car3.Print();
+
+            Console.WriteLine(car1 == car2);
+            Console.WriteLine(car2 == car3);
+        }
+    }
+}
